c7b0996daa920dc51c75ca2bbf25a721506b3754
Tamim Hasan <tamimh40@gmail.com>
2025-02-09 23:37:39 +0600

    update swagger security schemes

--- src/main/java/com/tamimSoft/fakeStore/config/SwaggerConfig.java
+++ src/main/java/com/tamimSoft/fakeStore/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.tamimSoft.fakeStore.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,17 +13,30 @@ import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI customSwaggerConfig() {
         return new OpenAPI()
-                .info(
-                        new Info().title("Fake Store API").description("Created by Tamim Hasan")
+                .info(new Info()
+                        .title("Fake Store API")
+                        .description("Created by Tamim Hasan")
+                        .version("1.0")
                 )
-                .servers(
-                        List.of(
-                                new Server().url("http://localhost:8080"),
-                                new Server().url("https://insightful-eagerness-production.up.railway.app")
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local Server"),
+                        new Server().url("https://insightful-eagerness-production.up.railway.app").description("Production Server")
+                ))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")
                         )
-                );
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth")); // Apply security globally
     }
 }
+
