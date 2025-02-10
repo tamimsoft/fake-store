4036b95f457c40799be257e47bf2ad3f1d17739d
Tamim Hasan <tamimh40@gmail.com>
2025-02-10 11:48:23 +0600

    Refactor sendMail method

--- src/main/java/com/tamimSoft/fakeStore/service/EmailService.java
+++ src/main/java/com/tamimSoft/fakeStore/service/EmailService.java
@@ -1,9 +1,10 @@
 package com.tamimSoft.fakeStore.service;
 
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,10 +16,12 @@ public class EmailService {
 
     public void sendMail(String to, String subject, String content) {
         try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(content);
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
             mailSender.send(message);
             log.info("Mail Send Success!");
         } catch (Exception e) {
