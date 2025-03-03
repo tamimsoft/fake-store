5863ef295d69fdacddc2cdab2c7af7144d69ec72
Tamim Hasan <tamimh40@gmail.com>
2025-03-03 15:10:00 +0600

    refactor:

--- src/main/java/com/tamimSoft/fakeStore/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tamimSoft/fakeStore/exception/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 import java.util.stream.Collectors;
 
@@ -15,13 +14,13 @@ public class GlobalExceptionHandler {
 
     // Handle InvalidOTPException
     @ExceptionHandler(InvalidOTPException.class)
-    public ResponseEntity<ApiResponse<?>> handleInvalidOTPException(InvalidOTPException ex, WebRequest request) {
+    public ResponseEntity<ApiResponse<?>> handleInvalidOTPException(InvalidOTPException ex) {
         return buildErrorResponse(HttpStatus.UNAUTHORIZED, ex.getMessage());
     }
 
     // Handle InvalidTokenException
     @ExceptionHandler(InvalidTokenException.class)
-    public ResponseEntity<ApiResponse<?>> handleInvalidTokenException(InvalidTokenException ex, WebRequest request) {
+    public ResponseEntity<ApiResponse<?>> handleInvalidTokenException(InvalidTokenException ex) {
         return buildErrorResponse(HttpStatus.UNAUTHORIZED, ex.getMessage());
     }
 
@@ -33,7 +32,7 @@ public class GlobalExceptionHandler {
 
     // Handle ResourceNotFoundException
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiResponse<?>> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+    public ResponseEntity<ApiResponse<?>> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
@@ -48,7 +47,7 @@ public class GlobalExceptionHandler {
 
     // Handle generic exceptions
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<?>> handleGenericException(Exception ex, WebRequest request) {
+    public ResponseEntity<ApiResponse<?>> handleGenericException(Exception ex) {
         return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred: " + ex.getMessage());
     }
 
--- src/main/java/com/tamimSoft/fakeStore/repository/UserRepository.java
+++ src/main/java/com/tamimSoft/fakeStore/repository/UserRepository.java
@@ -6,15 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
-import java.util.Set;
 
 public interface UserRepository extends MongoRepository<User, String> {
 
     Optional<User> findByUserName(String username);
 
     void deleteByUserName(String username);
-
-    Page<User> findAllByRoles(Set<String> roles, Pageable pageable);
 
     Page<User> findAllByRolesContaining(String role, Pageable pageable);
 
